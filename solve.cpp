ae647327e9a3c75ca95af8fe329e4a520a099afa
appleca0131 <appleca0131@gmail.com>
2016-04-04 19:19:57 +0800

    solve.cpp file.

---
+++ solve.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include "Sudoku.h"
+
+using namespace std;
+
+int main(){
+	Sudoku sudoku;
+	sudoku.readIn();
+	sudoku.solve();
+	return 0;
+}
